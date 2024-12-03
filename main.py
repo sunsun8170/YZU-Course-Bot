5cd8c30967f211e1543bae0912443948f3d3db32
sunsun8170 <s1101613@mail.yzu.edu.tw>
2024-12-03 11:04:44 +0800

    Add main.py
    1. Moved the main execution logic from bot.py to main.py to reduce complexity in bot.py.
    2. Reordered import statements for better organization.
    3. Updated the code to read from a YAML file instead of JSON.

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import yaml
+from src.bot import CourseBot
+import src.utils as utils
+
+try:
+  with open("course_list.yaml", "r", encoding="utf-8") as f:
+    usr_course_list = yaml.safe_load(f)["course_list"]
+except FileNotFoundError as e:
+  print(f"[ 找不到檔案 ] 請確認 course_list.yaml 是否存在!\n詳細資訊: {e}")
+  sys.exit(0)
+except yaml.YAMLError as e:
+  print(f"[ YAML 解析錯誤 ] 請確認 course_list.yaml 中語法是否正確!\n詳細資訊: {e}")
+  sys.exit(0)
+except Exception as e:
+  print(f"[ 未知的錯誤 ] 詳細資訊: {e}")
+  sys.exit(0)
+
+utils.enable_signal_handler()
+
+bot = CourseBot(usr_course_list)
+bot.run()
